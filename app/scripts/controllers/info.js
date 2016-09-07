6b3445cf27c77b8d841163fde61209e877dd0c7d
Madelene Campos <madelenecampos@gmail.com>
2016-09-07 14:38:50 -0400

    Adds scope in order to add info

--- app/scripts/controllers/info.js
+++ app/scripts/controllers/info.js
@@ -1,11 +1,6 @@
 'use strict';
 
 angular.module('desktopApp')
-  .controller('InfoCtrl', function () {
-    this.myInfo = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      "Hello, My name is Maddie"
-    ];
+  .controller('InfoCtrl', function ($scope) {
+    $scope.myInfo = "I am hungry";
   });
